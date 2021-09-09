d69189f8d24b632a597b69d12a0a8a0bd1645603
gha95 <gha95@uclive.ac.nz>
2021-09-09 16:26:26 +1200

    Preparing a basic mod with no functionality

---
+++ src/main/java/com/gha95/VariedGeodes/VariedGeodes.java
@@ -0,0 +1,28 @@
+package com.gha95.VariedGeodes;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("varied_geode")
+public class VariedGeodes {
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public VariedGeodes() {
+    	IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        // Register the setup method for modloading
+        modEventBus.addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+}
